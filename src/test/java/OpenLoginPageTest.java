337ce2d9e42acbb8d7fb5715c137a79f8f4ba203
nadins <nadin.slusarenko@gmail.com>
2015-06-17 15:44:32 +0300

    111

---
+++ src/test/java/OpenLoginPageTest.java
@@ -0,0 +1,56 @@
+package test.java;
+
+import main.java.pages.LuciannaLoginPage;
+import main.java.pages.LuciannaMainPage;
+import main.java.setup.LucianaHelper;
+import org.junit.AfterClass;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import static main.java.setup.SeleniumDriver.clearBrowserCache;
+import static main.java.setup.SeleniumDriver.getDriver;
+import static org.testng.Assert.assertTrue;
+
+/**
+ * Created by ad on 18.05.2015.
+ */
+public class OpenLoginPageTest {
+
+    public static final String EMAIL = "nadin.slusarenko1@gmail.com";
+    public static final String PASSWORD = "1qwerty12";
+    public static final String INCORRECT_PASSWORD = "111";
+    public static final String LOGIN = "NADIN-SLUSARENKO1";
+
+
+    @Test(groups = {"positive"})
+    public void loginWithCorrectCredentials() {
+        LuciannaLoginPage loginPage = new LuciannaMainPage().open().goToLoginPage();
+        loginPage.login(EMAIL, PASSWORD);
+        LucianaHelper.getScreenshot(getDriver());
+        assertTrue(loginPage.getWelcomeUser().getText().contains(LOGIN));
+    }
+
+    @AfterMethod
+    public static void clearCache() {
+        clearBrowserCache();
+    }
+
+    @Test(groups = {"negative"})
+    public void loginWithInCorrectCredentials() {
+
+        LuciannaLoginPage loginPage = new LuciannaMainPage().open().goToLoginPage();
+        loginPage.login(EMAIL, INCORRECT_PASSWORD);
+        ExpectedConditions.textToBePresentInElement(loginPage.getWoocommerceError(), LOGIN.toLowerCase());
+        LucianaHelper.getScreenshot(getDriver());
+        assertTrue(loginPage.getWoocommerceError().getText().contains(EMAIL.toLowerCase().substring(0, 17)));
+
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        clearBrowserCache();
+        getDriver().quit();
+        getDriver().close();
+    }
+}
---
+++ src/test/java/SearchTest.java
@@ -0,0 +1,39 @@
+package test.java;
+
+import main.java.pages.LuciannaMainPage;
+import org.junit.AfterClass;
+import org.testng.annotations.Test;
+
+import static main.java.setup.SeleniumDriver.clearBrowserCache;
+import static main.java.setup.SeleniumDriver.getDriver;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+/**
+ * Created by ad on 18.05.2015.
+ */
+public class SearchTest {
+
+    public static final String TEST = "Test";
+
+    @Test(groups = {"positive"})
+    public void searchTest() {
+        LuciannaMainPage page = new LuciannaMainPage().open();
+        page.mainPageSearch(TEST);
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        assertTrue(page.getTitle().getText().contains("TEST"));
+        assertEquals(page.getProducts().size(), 2);
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        clearBrowserCache();
+        getDriver().quit();
+        getDriver().close();
+
+    }
+}
